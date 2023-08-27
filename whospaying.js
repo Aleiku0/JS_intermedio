78382a28b2c25fc8ad6922ad3ffca8c18f0ba573
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-08-27 16:50:23 -0300

    Create whospaying.js
    random ex

---
+++ whospaying.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        //var names = ["Cappu", "Elsa", "Gigi", "Moonshine", "Apple Pie"];
+        var n = Math.floor(Math.random() * names.length);
+        return names[n] + ' is going to buy lunch today!';
+    
+    /******Don't change the code below*******/    
+    }
