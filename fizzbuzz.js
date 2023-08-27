970a7d09e2a023d0884b1c65244b1e8dc8f2697f
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-08-27 13:39:17 -0300

    Update fizzbuzz.js
    versión 1 fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,20 @@
 var output = [];
-var contador = 1;
+var count = 1;
 
-function fizzBuzz(){
+function fizzBuzz() {
 
-
-    console.log(output);    
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+        output.push("Fizz");
+    } else if (count % 5 === 0) {
+        output.push("Buzz");
+    } else {
+        output.push(count);
+    }
+    console.log(output);
+    count++;
 }
+
+// En consola ejecutar la función
+fizzBuzz();
